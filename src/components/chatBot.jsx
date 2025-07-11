c421fdae0e3efe551e3611e24239fbff6714491b
Ujjwal Kaushik <166251292+Ujjwal250802@users.noreply.github.com>
2025-07-11 14:23:18 +0530

    Updated chatBot.jsx

--- src/components/chatBot.jsx
+++ src/components/chatBot.jsx
@@ -21,7 +21,7 @@ const Chatbot = () => {
     setMessages((prev) => [...prev, { sender: "ChatSphere AI", text: "typing..." }]);
 
     try {
-      const res = await fetch(`${import.meta.env.MODE === "development" ? "https://chat-sphere-backend-riae.onrender.com"}/api/chatbot`, {
+      const res = await fetch("https://chat-sphere-backend-riae.onrender.com/api/chatbot"{
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: userText }),
